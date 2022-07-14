2a75c3987929c4c220bc6a2b4098a19d8c5b57da
gcyrillus <1832331+gcyrillus@users.noreply.github.com>
2022-07-14 20:52:10 +0200

    Add files via upload

---
+++ delaySiteCSS/delaySiteCSS.php
@@ -0,0 +1,25 @@
+<?php
+class delaySiteCSS extends plxPlugin {	 
+	const BEGIN_CODE = '<?php' . PHP_EOL;
+	const END_CODE = PHP_EOL . '?>';
+    
+	public function __construct($default_lang) {
+		
+		# appel du constructeur de la classe plxPlugin (obligatoire)
+		parent::__construct($default_lang);
+		
+		# déclaration des hooks
+		$this->addHook('plxShowPluginsCss', 'plxShowPluginsCss');
+		
+
+	}	
+	public function plxShowPluginsCss(){ 
+	echo '<link rel="stylesheet" type="text/css" href="data.site" media="none" onload="if(media!=\'all\')media=\'all\'"  />';
+				echo self::BEGIN_CODE;
+?>
+	return true;
+<?php
+		echo self::END_CODE;        
+       }
+}
+?>
